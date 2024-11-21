cc6c0f48502f71dd74957aeac4b990a9776cc1af
Ethlind <ethlind_leo_yee_qian@connect.it.edu.sg>
2024-11-21 13:23:58 +0800

    Add project files.

---
+++ Data/MemberDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ethlind_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ethlind_Project.Data
+{
+    public class MemberDbContext : DbContext
+    {
+        public MemberDbContext(DbContextOptions<MemberDbContext> options)
+        : base(options)
+        { 
+        }
+
+        public DbSet<Booking>? Booking { get; set; }
+    }
+}
---
+++ Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ethlind_Project.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
